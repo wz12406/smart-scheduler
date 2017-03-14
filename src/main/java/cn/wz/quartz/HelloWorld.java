96e40b07090b80a55f6eb76159e9d0cd05c331e1
wangzhen <768834204@qq.com>
2017-03-14 18:03:51 +0800

    先来个hello world！

---
+++ src/main/java/cn/wz/quartz/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.wz.quartz;
+
+/**
+ * @author wz
+ * @date 2017/3/14 18:02
+ * @desc
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
